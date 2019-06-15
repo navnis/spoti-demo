b31412d54b675f20ee9ab16c119539f36c229cef
navnis <nischal.912@gmail.com>
2019-06-16 02:32:43 +0530

    bug fixed

--- client/src/component/searchArtist.js
+++ client/src/component/searchArtist.js
@@ -7,44 +7,63 @@ const spotifyWebApi = new Spotify()
 class Search extends Component {
 constructor(props){
   super(props);
-    const params = this.getHashParams()
+    // const params = this.getHashParams()
 
   this.state ={
-    loggedIn : params.access_token ? true : false,
+    // loggedIn : params.access_token ? true : false,
     nowPlaying :{ 
         name : "Not Checked",
         image : ""
     }
   }
 
-  if(params.access_token){
-      spotifyWebApi.setAccessToken(params.access_token)
-  }
+//   if(params.access_token){
+//       console.log('inside testing access token0', params.access_token)
+//       this.setState({
+//           access_token: params.access_token
+//       }) 
+      
+      
+//       spotifyWebApi.setAccessToken(params.access_token)
+//   }
 }
 
+componentDidMount(){
+    this.getHashParams()
+}
 //copied from auth-server/authorization_code/public/index.html
-getHashParams() {
+getHashParams = () => {
     var hashParams = {};
     var e, r = /([^&;=]+)=?([^&;]*)/g,
         q = window.location.hash.substring(1);
     while ( e = r.exec(q)) {
        hashParams[e[1]] = decodeURIComponent(e[2]);
     }
-    return hashParams;
+    console.log('testing in hash params', hashParams)
+    this.setState({
+        access_token: hashParams.access_token
+    })
+    // return hashParams;
   }
 
 
-  getNowPlaying(){
+  getNowPlaying = () => {
+    //   this.getHashParams()
+      console.log('testing inside get now playing', this.state.access_token)
+      spotifyWebApi.setAccessToken(this.state.access_token)
+    //   .then(res => con   le.   log('inside res for set token', res))
     spotifyWebApi.getMyCurrentPlaybackState()
-    .then( (response) => {
+    .then(res => {
+        console.log('testing response', res)
         // this.setState({
         //     nowPlaying :{
         //         name: response.item.name,
         //         image: response.item.album.image[0].url
         //     }
         // })
-        console.log(response)
+        // console.log(response)
     })
+    .catch(er => console.log('testing', er))
   }
 
 render(){
